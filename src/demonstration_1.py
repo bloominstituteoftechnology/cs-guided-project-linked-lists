496c62450c3b2a9c15d67482c1dcbc93ad2a330b
willwearing <willwearing@gmail.com>
2020-12-08 12:05:43 -0700

    lesson finished

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,42 +1,67 @@
 """
 Given only a reference to a specific node in a linked list, delete that node from a singly-linked list.
-
 Example:
-
-The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked list by calling the function `delete_node`. It is your job to write the `delete_node` function.
-
+The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked
+list by calling the function `delete_node`. It is your job to write the `delete_node` function.
 ```python
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
         self.next  = None
-
 x = LinkedListNode('X')
 y = LinkedListNode('Y')
 z = LinkedListNode('Z')
-
 x.next = y
 y.next = z
-
 delete_node(y)
 ```
-
 *Note: We can do this in O(1) time and space! But be aware that our solution will have some side effects...*
 """
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
         self.next  = None
-
-def delete_node(delete_this_node):
-    # Your code here
-
-
+def delete_node(node_to_delete):
+    """
+    Input: a reference to a node in a linked list that we want
+    to delete from the linked list
+    Output: this function will mutate the linked list but it
+    won't return anything
+    One way we can delete a node from a linked list is to change
+    the node that refers to the node we want to delete so that it
+    points to the node we want to delete next node
+    We need access to the node that refers to the node we want to
+    delete
+    So we're replacing the 'node_to_delete', and then deleting the 
+    next node
+    1. Changed the 'node_to_delete' value to its next node's value
+    2. Remove the 'node_to_delete' next node by having 'node_to_delete'
+       next refer to the next node's next
+    """
+    next_node = node_to_delete.next
+    # set the 'node_to_delete' value to its next node's value
+    node_to_delete.value = next_node.value
+    # set 'node_to_delete' next to the next node's next
+    node_to_delete.next = next_node.next
+# define a function that prints every node
+# in the linked list starting with the node
+def print_ll(node):
+    # init a new reference that will keep track
+    # of where we are in the traversal
+    current = node
+    # traverse the linked list until
+    # we get to the end
+    while current is not None:
+        # print the value of each node along the way
+        print(current.value)
+        # update current to refer to the next node
+        # in the linked list
+        current = current.next
 x = LinkedListNode('X')
 y = LinkedListNode('Y')
 z = LinkedListNode('Z')
-
 x.next = y
 y.next = z
-
+print_ll(x)
 delete_node(y)
+print_ll(x)
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -2,13 +2,13 @@
 Given a reference to the head node of a singly-linked list, write a function
 that reverses the linked list in place. The function should return the new head
 of the reversed list.
-
+​
 In order to do this in O(1) space (in-place), you cannot make a new list, you
 need to use the existing nodes.
-
+​
 In order to do this in O(n) time, you should only have to traverse the list
 once.
-
+​
 *Note: If you get stuck, try drawing a picture of a small linked list and
 running your function by hand. Does it actually work? Also, don't forget to
 consider edge cases (like a list with only 1 or 0 elements).*
@@ -17,7 +17,66 @@ class LinkedListNode():
     def __init__(self, value):
         self.value = value
         self.next  = None
-
+​
 def reverse(head_of_list):
-    # Your code here
-
+    '''
+    Input: head of the linked list we want to reverse 
+    Output: the head of the reversed linked list 
+​
+    We need to traverse the linked list and flip all their next arrows
+    What do we need to keep track of in order to flip each arrow? 
+​
+    We need to keep track of three things: the current node, the next node, 
+    and a reference to the previous node 
+​
+    Traverse through our linked list 
+        set current's next to refer to prev
+        update our references to each point to their respective next nodes 
+​
+    At the end, we need to return the new head 
+    '''
+    # init our three pointers 
+    current = head_of_list
+    prev = None 
+​
+    # traverse so long as current is referring to a Node 
+    while current is not None:
+        next = current.next
+        # flip current's arrow around 
+        current.next = prev
+​
+        # update our three pointers 
+        prev = current 
+        current = next 
+    
+    # the `prev` pointer is now referring to the new head 
+    return prev 
+​
+# initialize a few LinkedListNodes
+x = LinkedListNode('X')
+y = LinkedListNode('Y')
+z = LinkedListNode('Z')
+​
+# connect the LinkedListNodes together 
+x.next = y
+y.next = z
+​
+# define a function that prints every node 
+# in the linked list starting with the input node 
+def print_ll(node):
+    # init a new reference that will keep track 
+    # of where we are in the traversal 
+    current = node 
+    # traverse the linked list until 
+    # we get to the end 
+    while current is not None:
+        # print the value of each node along the way 
+        print(current.value)
+        # update current to refer to the next node
+        # in the linked list 
+        current = current.next
+​
+print_ll(x)
+print('====')
+new_head = reverse(x)
+print_ll(new_head)
